999730f8468ec5191b57659e2c62001cc4ad2c42
Fabs90 <fabianjuliansyah89@gmail.com>
2022-08-18 13:08:38 +0700

    Membuat tampilan nevbar dan jumbotron

--- halaman_tumpeng.php
+++ halaman_tumpeng.php
@@ -1,2 +1,99 @@
 <?php
 require 'connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Bootstrap demo</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+      crossorigin="anonymous"
+    />
+    <!-- My CSS -->
+    <link rel="stylesheet" href="menu_tumpeng.css" />
+
+    <!-- My icon -->
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
+
+    <!-- Icon Footer -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav
+      class="navbar navbar-expand-lg navbar-dark shadow-sm fixed-top bg-dark"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="#"
+          ><img
+            src="img/Restaurant_Premium_Food_Logo_Template-removebg-preview.png"
+            alt="Logo"
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="halaman_tumpeng.php">Menu Tumpeng</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="halaman_nasiBox.php">Menu Nasi Box</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#footer">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="" href="#"
+                ><button class="tombol btn btn-outline-warning">
+                  Login
+                </button></a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <!-- Jumbotron -->
+    <section class="jumbotron">
+        <div class="container-fluid text-center pt-10">
+    <div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-black">
+  <h1 class="mb-3 h2">Tumpeng</h1>
+  <p>
+  Berbagai varian ukuran dan kemasan tumpeng untuk acara spesialmu
+  </p>
+  </div>
+</div>
+    </section>
+    <!-- Akhir jumbotron -->
+
+    <!-- Display Tumpeng -->
+
+    <!-- Akhir display tumpeng -->
+
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
