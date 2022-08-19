7909d4df2b12aac060dc914caabf71b1077fffd2
Fabs90 <fabianjuliansyah89@gmail.com>
2022-08-19 21:24:48 +0700

    Menambahkan function yang heading ke halaman home

--- validation.php
+++ validation.php
@@ -1,5 +1,10 @@
-<?php 
+<?php
 require 'connection.php';
+session_start();
 
-
-?>
+// (Klik Pesan dari halaman Landing Page)
+// Jika tombol kirim awal di klik
+if (isset($_POST['kirim_awal'])) {
+    // Pindah ke halaman home
+    header("Location:home.php");
+}
