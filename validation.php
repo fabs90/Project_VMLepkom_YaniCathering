82a8c865d418d3e528e9596abec16d335a22567b
Fabs90 <fabianjuliansyah89@gmail.com>
2022-08-20 01:15:47 +0700

    Menghapus array error dan menambahkan function add item

--- validation.php
+++ validation.php
@@ -1,16 +1,17 @@
 <?php
 require 'connection.php';
+
+// Start session
 session_start();
-$errors = array();
 
 // (Klik Pesan dari halaman Landing Page)
-// Jika tombol kirim awal di klik
+// Jika tombol pesan display item klik
 if (isset($_POST['kirim_awal'])) {
     // Pindah ke halaman home
     header("Location:home.php");
 }
 
-// Validasi Login
+// (validasi Login)
 if (isset($_POST['submit'])) {
 
     // Ambil input data dari form dan kasih sql injection
@@ -47,4 +48,85 @@ if (isset($_POST['submit'])) {
     }
 }
 
-// Validasi
+// (Validasi Add To Cart TUMPENG)
+if (isset($_POST['kirim_tumpeng'])) {
+    // Ambil data dari yg user klik
+    $id = $_GET['id'];
+    $nama = $_POST["nama"];
+    $harga = number_format($_POST["harga"], 2);
+    $jumlah_barang = $_POST["jumlah_barang"];
+
+    // Kalau blom ada session_cart, buat array nya
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'][] = array(
+            'id' => $id,
+            'nama' => $nama,
+            'harga' => $harga,
+            'jumlah_barang' => $jumlah_barang,
+        );
+    } else {
+        // Ambil semua ID saja dari array session_cart
+        $SESSION_CART_ID = array_column($_SESSION['cart'], 'id');
+        // Kalo id nya gaada di array, masukin ke array
+        if (!in_array($id, $SESSION_CART_ID)) {
+            $_SESSION['cart'][] = array(
+                'id' => $id,
+                'nama' => $nama,
+                'harga' => $harga,
+                'jumlah_barang' => $jumlah_barang,
+            );
+        }
+
+    }
+
+}
+
+// (Validasi Add To Cart NASIBOX)
+if (isset($_POST['kirim_nasibox'])) {
+    // Ambil data dari yg user klik
+    $id = $_GET['id'];
+    $nama = $_POST["nama"];
+    $harga = number_format($_POST["harga"], 2);
+    $jumlah_barang = $_POST["jumlah_barang"];
+
+    // Kalau blom ada session_cart, buat array nya
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'][] = array(
+            'id' => $id,
+            'nama' => $nama,
+            'harga' => $harga,
+            'jumlah_barang' => $jumlah_barang,
+        );
+    } else {
+        // Ambil semua ID saja dari array session_cart
+        $SESSION_CART_ID = array_column($_SESSION['cart'], 'id');
+        // Kalo id nya gaada di array, masukin ke array
+        if (!in_array($id, $SESSION_CART_ID)) {
+            $_SESSION['cart'][] = array(
+                'id' => $id,
+                'nama' => $nama,
+                'harga' => $harga,
+                'jumlah_barang' => $jumlah_barang,
+            );
+        }
+
+    }
+
+}
+
+// (Menghapus Item dari Cart)
+if (isset($_GET["action"])) {
+    if (isset($_GET['action']) == 'remove') {
+        // Menangkap id dari url
+        $id = $_GET["id"];
+        // Loop array session_cart
+        foreach ($_SESSION['cart'] as $key => $value) {
+            // Jika value[id] == id di url
+            if ($value['id'] == $id) {
+                // Menghapus dgn cara unset key yg sesuai sama id
+                unset($_SESSION['cart'][$key]);
+                $success['delete'] = "Sucessfully Delete";
+            }
+        }
+    }
+}
