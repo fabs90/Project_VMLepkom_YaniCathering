8bb12d2e145c4dbbe0b01163055c0eab6bab8d2a
Fabs90 <fabianjuliansyah89@gmail.com>
2022-11-03 13:15:51 +0700

    menambah fungsi error handling

--- validation.php
+++ validation.php
@@ -215,3 +215,31 @@ if (isset($_POST['kirim_data'])) {
     }
 
 }
+
+// (Error Handling)
+# sembunyikan semua pesan error bawaan PHP
+error_reporting(0);
+
+function tanganiError($level, $message, $file, $line)
+{
+    echo "<div style='padding: 2rem; background: rgba(200, 0, 0, 0.5); color: white'>";
+    echo "<b>Terjadi Error</b>";
+    echo "<p>[{$level}] {$message} - {$file}:{$line}</p>";
+    echo "</div>";
+
+    return true;
+}
+
+register_shutdown_function(function () {
+    if (error_get_last()) {
+        # ambil error terakhir
+        $error = (object) error_get_last();
+
+        # hapus semua output sebelum terjadi error
+        ob_end_clean();
+
+        tanganiError(
+            $error->type, $error->message, $error->file, $error->line
+        );
+    }
+});
